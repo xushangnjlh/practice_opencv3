05ea8dbe86d0c668e451f8085359976d44a3875a
xushangnjlh <xushangnjlh@gmail.com>
2017-05-16 16:12:12 +0800

    first

---
+++ src/feature_training.cpp
@@ -0,0 +1,20 @@
+#include "DBoW3/DBoW3.h"
+// #include <opencv2/core/core.hpp>
+// #include <opencv2/highgui/highgui.hpp>
+// #include <opencv2/features2d/features2d.hpp>
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+  cout << "Reading images..." << endl;
+  vector<Mat> images;
+  for(int i=0; i<10; i++)
+  {
+    string path = "./data"+to_string(i+1)+".png";
+  }
+}
