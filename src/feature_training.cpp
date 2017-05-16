9f673b95ee2902272e9742edb04bf4e609c1063e
xushangnjlh <xushangnjlh@gmail.com>
2017-05-16 18:33:03 +0800

    first

--- src/feature_training.cpp
+++ src/feature_training.cpp
@@ -1,7 +1,7 @@
 #include "DBoW3/DBoW3.h"
-// #include <opencv2/core/core.hpp>
-// #include <opencv2/highgui/highgui.hpp>
-// #include <opencv2/features2d/features2d.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/features2d/features2d.hpp>
 #include <iostream>
 #include <vector>
 #include <string>
@@ -12,7 +12,7 @@ using namespace std;
 int main()
 {
   cout << "Reading images..." << endl;
-  vector<Mat> images;
+  vector<Mat> images; 
   for(int i=0; i<10; i++)
   {
     string path = "./data"+to_string(i+1)+".png";
